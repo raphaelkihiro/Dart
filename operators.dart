313f4c005653dd92bea509619ddb659b28a43ea6
raphael kihiro <kihirongigi@gmail.com>
2025-10-06 12:15:48 +0300

    dart operators

---
+++ operators.dart
@@ -0,0 +1,20 @@
+/**
+the operators in dart and their functionality.
+a <= b a is less than or equal to b 
+b >= a b is greater than or equal to b 
+logical operators
+! -	inverts the following expression (changes false to true, and vice versa)
+|| -	logical OR
+&& - logical AND
+ */
+void main() {
+    int a = 7;
+    int b = 3;
+    print(a + b);
+    print(a - b);
+    print(a * b);
+    print(a / b);  // division
+    print(a ~/ b); // Result is a integer 
+    print(5 % 2); // Result  to the reminder (modulors)
+
+}

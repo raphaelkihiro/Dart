e4dbb9a5521bd2f7fc76a659a45156d1cf1a57cd
raphael kihiro <kihirongigi@gmail.com>
2025-10-06 11:19:35 +0300

    learing dart basics

--- intro.dart
+++ intro.dart
@@ -1,7 +1,31 @@
+
+// we have to ensure that we put the semi-colon at the end of every variable 
+
 void main() {
+  // look up function 
   print('Hello, World!');
+  // if you intended not to change a variable we use const or final
+  final day = 'Monstag';
+  print(day);
+  const numberOfPeople = 15;
+   print(numberOfPeople);
+
+  // how to introduce variable in dart you either use the (var) keyword or we introduce the datatype name
   var name = "Bob";
   print(name);
-  Object firstname = 'Ngaji','Paul', 1;
-  print(firstname);
+  // the variables in dart also follows the carmel case naming convetion
+  Object firstName = ('Ngaji','Paul', 1);
+  print(firstName);
+  String lastName = "Ngigi";
+  print(lastName);
+  // we add a question mark to allow for a varaiable to be nulluble
+    String ? year;// nullable
+  print(year);
+  // String food  //unnullable variable
+
+  int? lineCount = 12;
+  assert(lineCount == null);  
 }
+
+
+

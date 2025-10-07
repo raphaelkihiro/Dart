dfab444913185ecf3c15269587fe12d947ce929f
raphael kihiro <kihirongigi@gmail.com>
2025-10-07 09:28:55 +0300

    dart data types

---
+++ datatypes.dart
@@ -0,0 +1,36 @@
+/**
+data types in dart 
+(Numbers,  Strings, Booleans, Lists, Maps, Dynamics)
+ */
+void main(){
+//   1. Strings 
+/**
+the value is encloused in double or single quotations marks \
+ */
+String name = 'Raph';
+print ("Name: $name" );
+
+// 2. Numbers
+/** the data type is divided into 2 doubles and integers 
+a. integers are whole figures
+b. doubles are numbers with decimal points 
+ */
+//  integers 
+ int age = 24; 
+ print ("Age:$age");
+
+// doubles 
+double height = 181;
+print ("Height: $height");
+
+/**
+3. Booleans
+this are data types that translate to either true or false 
+ */
+bool married = true;
+print ("Married : $married");
+
+bool old = false;
+print("Eldery: $old");
+
+}
